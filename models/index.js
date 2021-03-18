907ec7acb5ad4e739b1396dcdb2b57d656db3aa2
Maryum Bokhari <maryumbokhari97@gmail.com>
2021-03-18 15:16:26 +0000

    added foreign keys

--- models/index.js
+++ models/index.js
@@ -4,18 +4,26 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
+// DEFINE RELATIONSHIPS BETWEEN MODELS HERE:
+
 // Products belongsTo Category
-Product.belongsTo(Category);
+Product.belongsTo(Category, {
+  foreignKey: 'category_id'
+});
 
 // Categories have many Products
-Category.hasMany(Product);
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  onDelete: 'SET NULL'
+});
 
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
   through: {
     model: ProductTag,
     unique: false
-  }
+  },
+  foreignKey: 'product_id'
 })
 
 // Tags belongToMany Products (through ProductTag)
@@ -23,7 +31,8 @@ Tag.belongsToMany(Product, {
   through: {
     model: ProductTag,
     unique: true
-  }
+  },
+  foreignKey: 'tag_id'
 })
 
 module.exports = {
