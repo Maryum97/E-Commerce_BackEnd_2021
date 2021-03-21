fad4f54d6aa2ea66274715249bffb707d61d5290
Maryum Bokhari <maryumbokhari97@gmail.com>
2021-03-21 19:26:50 +0000

    edited code for PUT request

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -105,7 +105,7 @@ router.put('/:id', (req, res) => {
   })
     .then((product) => {
       // find all associated tags from ProductTag
-      return ProductTag.findAll({ where: { product_id: req.params.id } });
+      return ProductTag.findAll(req.body, { where: { product_id: req.params.id } });
     })
     .then((productTags) => {
       // get list of current tag_ids
